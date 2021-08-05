6e22484e5250115c1ef5c7df1151883eaeb43d01
yoyo930021 <yoyo930021@gmail.com>
2021-08-05 23:35:17 +0800

    feat: add http log output

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const { resolve } = require("path")
+const logger = require("morgan")
 
 const port = Number.isNaN(Number(process.env.PORT)) ? 3000 : Number(process.env.PORT)
 
 const app = express()
+
+app.use(logger('dev'))
 
 const STATIC = resolve(__dirname, './app')
 const INDEX = resolve(STATIC, 'index.html')
