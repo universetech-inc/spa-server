99dd11f1e737c8e7bbd85b76abaf98078f086a06
yoyo930021 <yoyo930021@gmail.com>
2021-12-02 17:06:15 +0800

    feat: cache-control for spa

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ app.use(logger('dev'))
 
 const STATIC = resolve(__dirname, './app')
 const INDEX = resolve(STATIC, 'index.html')
-app.use(express.static(STATIC))
-app.get('*', (_, res) => {
-  res.sendFile(INDEX)
-})
+
+const sendIndex = (_, res) => {
+  res.header({ 'cache-control': 'no-store,max-age=0' }).sendFile(INDEX)
+}
+
+app.get('/', sendIndex)
+app.get('/index.html', sendIndex)
+app.use(express.static(STATIC, { cacheControl: true, maxAge: '4w' }))
+app.get('*', sendIndex)
 
 app.listen(port, () => { console.log(`Listening on ${port}`) })
